eb603eceec3ddb8cb7a17d9290e807f7a1fdf557
Lindsey Brown <libersword@gmail.com>
2019-10-21 19:38:37 -0600

    modified seatgeek api and added file with venues object

---
+++ venues.js
@@ -0,0 +1,30 @@
+var venues = [
+  {
+    venueName: "Pepsi Center",
+    venueID: 33
+  },
+  {
+    venueName: "The Fillmore Auditorium",
+    venueID: 33
+  },
+  {
+    venueName: "The Ogden Theater",
+    venueID: 33
+  },
+  {
+    venueName: "The Bluebird Theater",
+    venueID: 33
+  },
+  {
+    venueName: "Mission Ballroom",
+    venueID: 33
+  },
+  {
+    venueName: "Summit Music Hall",
+    venueID: 33
+  },
+  {
+    venueName: "Summit Music Hall",
+    venueID: 33
+  },
+]
