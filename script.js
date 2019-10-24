8ed6679fad7667a45db5491d0c055e784c81f036
ArtVandelay400 <anthony.patak@gmail.com>
2019-10-23 18:18:25 -0600

    updating event listener to feature 'touchstart'

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ var initMap = function() {
   });
 }
 
-$(document).on("click", ".btn-floating", function(){
+$(document).on("click touchstart", ".btn-floating", function(){
    lat= parseFloat($(this).attr("data-lattitude"), 10);
    lon= parseFloat($(this).attr("data-longitude"), 10)
    map = new google.maps.Map(document.getElementById("map"), {
